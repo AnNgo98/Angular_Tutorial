f2f7a60ea67c16a33534aeed9c2216e82ee7d8b3
AnNgo98 <40728998+AnNgo98@users.noreply.github.com>
2019-09-29 21:16:09 +0700

    Add function
    - Update API
    - Create UI angular 8

---
+++ Payment-Detail-Register/Payment-Detail-Register-API/Payment-Detail-Register-API/Models/PaymentDetail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Payment_Detail_Register_API.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        //[Column(Order = 1)]
+        public int PMId { get; set; }
+        [Required]
+        [Column (TypeName = "nvarchar(100)")]
+        public string MyProperty { get; set; }
+        [Required]
+        [Column (TypeName ="varchar(16)")]
+        public string CardNumber { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(5)")]
+        public string ExpirationDate { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(3)")]
+        public string CVV { get; set; }
+    }
+}
